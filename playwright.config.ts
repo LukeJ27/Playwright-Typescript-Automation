9b60514cebcc0f24d0ddb901e3261b010fc9f66a
LukeJ27 <lukejames027@gmail.com>
2025-11-14 18:23:03 +0000

    Adding videos and reporting to GHA runs

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,12 +4,16 @@ export default defineConfig({
   testDir: './tests',
   timeout: 30000,
   retries: 0,
-  reporter: [['list']],
+  reporter: [
+    ['html'],
+    ['list'],
+  ],
   use: {
     headless: false,
     viewport: { width: 1280, height: 720 },
     actionTimeout: 10000,
     ignoreHTTPSErrors: true,
-    video: 'retain-on-failure',
+    video: 'on',
+    trace: 'on',
   },
 });
